a74994908f801c5b440f89aedfa75caf7a60ccac
Ansonhkg <ansonox@gmail.com>
2023-01-31 15:41:33 +0000

    add: utils example

--- apps/playground-nextjs/pages/index.tsx
+++ apps/playground-nextjs/pages/index.tsx
@@ -1,11 +1,17 @@
 import { BrandLogo, Button } from "ui";
+import { hello } from "@lit-dev/utils";
+import { useEffect } from "react";
 
 export default function Web() {
+  useEffect(() => {
+    hello();
+  });
+
   return (
     <div>
       <h1>Web</h1>
       <Button />
-      <BrandLogo/>
+      <BrandLogo />
     </div>
   );
 }
---
+++ packages/utils/index.tsx
@@ -0,0 +1,3 @@
+export const hello = async () => {
+  console.log("World!");
+};
