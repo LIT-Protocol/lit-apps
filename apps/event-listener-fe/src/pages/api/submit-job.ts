d0cf1de1a3704d3404e9954e54ab8271c18ca309
Ansonhkg <ansonox@gmail.com>
2023-02-08 20:52:13 +0000

    WIP

--- apps/event-listener-fe/src/pages/api/submit-job.ts
+++ apps/event-listener-fe/src/pages/api/submit-job.ts
@@ -168,8 +168,8 @@ export default async function handler(
 
   //  reset
   // clear queue
-  // await blockEventWaitingList.empty();
-  // await blockEventProcessingList.empty();
+  await blockEventWaitingList.empty();
+  await blockEventProcessingList.empty();
 
   // ---------------------------------------------------
   //          Add to Block Event Waiting List
--- apps/event-listener-fe/src/pages/tasks.tsx
+++ apps/event-listener-fe/src/pages/tasks.tsx
@@ -1,18 +1,26 @@
 import { safeFetch } from "@lit-dev/utils";
 import { useEffect, useState } from "react";
+import { sha256 } from "ethers/lib/utils";
+import { LitLoading } from "ui";
 
 export function Tasks() {
   const [data, setData] = useState<any>();
 
   useEffect(() => {
+    var interval: any;
+    
     if (!data) {
       (async () => {
-        const res = await safeFetch("/api/get-jobs");
+        // run every 10 seconds
+        interval = setInterval(async () => {
+          const res = await safeFetch("/api/get-jobs");
 
-        console.log("res:", res.data);
-        setData(res.data);
+          console.log("res:", res.data);
+          setData(res.data);
+        }, 10000);
       })();
     }
+    return () => clearInterval(interval);
   }, []);
 
   return (
@@ -20,31 +28,44 @@ export function Tasks() {
       <h1>Tasks</h1>
       <div className="flex flex-col gap-12">
         {!data ? (
-          ""
+          <LitLoading icon="lit-logo" text="Fetching tasks..." />
         ) : (
           <div className="flex flex-col">
             <h3 className="mb-12">Waiting jobs: {data.waitingJobs.length}</h3>
             {data.waitingJobs.map((job: any) => {
+              //   // convert job.data.name to buffer
+              //   const buf = Buffer.from(job.data.name, "base64");
+              //   // convert buffer to hash
+              //   const hash = sha256(buf);
+
               return (
                 <div className="pkp-card">
                   <div>Job ID: {job.id}</div>
-                  <div>Event Type Type: {job.data.jobData.eventType}</div>
                   <div>Job Name: {job.data.name}</div>
+                  <div>Event Type: {job.data.jobData.eventType}</div>
+                  <div>
+                    Event Params: {JSON.stringify(job.data.jobData.eventParams)}
+                  </div>
+                  <div>Ipfs ID: {job.data.jobData.ipfsId}</div>
                 </div>
               );
             })}
-            <h3 className="mb-12">Processing jobs: {data.processingJobs.length}</h3>
+            <h3 className="mb-12">
+              Processing jobs: {data.processingJobs.length}
+            </h3>
             {data.processingJobs.map((job: any) => {
               return (
                 <div className="pkp-card">
                   <div>Job ID: {job.id}</div>
-                  <div>Job Type: {job.type}</div>
-                  <div>Job Status: {job.status}</div>
+                  <div>Event Type: {job.data.jobData.eventType}</div>
+                  <div>Job Name: {job.data.name}</div>
                 </div>
               );
             })}
 
-            <h3 className="mb-12">Completed jobs: {data.completedJobs.length}</h3>
+            <h3 className="mb-12">
+              Completed jobs: {data.completedJobs.length}
+            </h3>
             {/* {data.completedJobs.map((job: any) => {
               return (
                 <div className="pkp-card">
