38a5e32d18c315443120a6fdfee0ae8182a507ec
Ansonhkg <ansonox@gmail.com>
2023-02-22 17:15:03 +0000

    add plausible

--- apps/event-listener-fe/src/pages/_app.tsx
+++ apps/event-listener-fe/src/pages/_app.tsx
@@ -5,26 +5,29 @@ import { LitHeaderV1, PKPConnectionContext, ThemeA, Web3Auth } from "ui";
 import { Toaster } from "react-hot-toast";
 import { hotjar } from "react-hotjar";
 import { useEffect } from "react";
+import PlausibleProvider from "next-plausible";
 
 export default function App({ Component, pageProps }: AppProps | any) {
   const appName = `Lit Actions Event Listener`;
 
   useEffect(() => {
-    hotjar.initialize(3378726, 6);
+    hotjar.initialize(3379254, 6);
   }, []);
 
   return (
-    <Web3Auth>
-      <PKPConnectionContext>
-        <Toaster />
-        <Head>
-          <title>{appName}</title>
-        </Head>
-        <ThemeA className="app" data-lit-theme="purple">
-          <LitHeaderV1 title={appName} />
-          <Component {...pageProps} />
-        </ThemeA>
-      </PKPConnectionContext>
-    </Web3Auth>
+    <PlausibleProvider domain="event-listener.litgateway.com">
+      <Web3Auth>
+        <PKPConnectionContext>
+          <Toaster />
+          <Head>
+            <title>{appName}</title>
+          </Head>
+          <ThemeA className="app" data-lit-theme="purple">
+            <LitHeaderV1 title={appName} />
+            <Component {...pageProps} />
+          </ThemeA>
+        </PKPConnectionContext>
+      </Web3Auth>
+    </PlausibleProvider>
   );
 }
