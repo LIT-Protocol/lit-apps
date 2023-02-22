0cf6e4c8fddbb646805b853cf062d013b76ad936
Ansonhkg <ansonox@gmail.com>
2023-02-22 12:41:24 +0000

    add: hotjar

--- apps/event-listener-fe/src/pages/_app.tsx
+++ apps/event-listener-fe/src/pages/_app.tsx
@@ -3,9 +3,15 @@ import Head from "next/head";
 import "ui/theme.purple.css";
 import { LitHeaderV1, PKPConnectionContext, ThemeA, Web3Auth } from "ui";
 import { Toaster } from "react-hot-toast";
+import { hotjar } from "react-hotjar";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps | any) {
   const appName = `Lit Actions Event Listener`;
+
+  useEffect(() => {
+    hotjar.initialize(3378726, 6);
+  }, []);
 
   return (
     <Web3Auth>
