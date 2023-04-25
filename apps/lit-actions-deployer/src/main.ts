91637e7de1d44b43e1c10f9708ed652d6dfae36d
Ansonhkg <ansonox@gmail.com>
2023-04-25 21:10:23 +0100

    feat: add rate limit

--- apps/lit-actions-deployer/src/main.ts
+++ apps/lit-actions-deployer/src/main.ts
@@ -1,13 +1,22 @@
 import { uploadLitAction } from "@lit-dev/utils";
 import express, { Express, Request, Response } from "express";
+import rateLimit from "express-rate-limit"; // Import the express-rate-limit middleware
 import dotenv from "dotenv";
+
 const PORT = process.env.PORT || 3000;
 
 dotenv.config();
 
 const app: Express = express();
 
+const apiLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 10, // Limit each IP to 10 requests per windowMs
+  message: "Too many requests from this IP, please try again later.",
+});
+
 app.use(express.json());
+app.use(apiLimiter); // Use the rate-limiting middleware
 
 app.post("/", async (req: Request, res: Response) => {
   console.log("Received data:", req.body);
