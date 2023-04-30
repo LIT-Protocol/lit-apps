7d6fae5093a8fbe14ce837d539911218063f0ac2
Ansonhkg <ansonox@gmail.com>
2023-04-30 22:14:05 +0100

    feat: simplified heroku-postbuild.js

--- heroku-postbuild.js
+++ heroku-postbuild.js
@@ -2,19 +2,4 @@ const { execSync } = require("child_process");
 
 const appEnv = process.env.APP_ENV;
 
-switch (appEnv) {
-  case "event-listener-fe":
-    execSync("yarn run build:el:fe", { stdio: "inherit" });
-    break;
-  case "event-listener-be":
-    execSync("yarn run build:el:be", { stdio: "inherit" });
-    break;
-  case "lit-actions-deployer":
-    execSync("yarn run build:lit-actions-deployer", { stdio: "inherit" });
-    break;
-  case "lit-general-worker":
-    execSync(`yarn turbo run build --filter ${appEnv}`, { stdio: "inherit" });
-    break;
-  default:
-    console.log("No environment detected, please set APP_ENV");
-}
+execSync(`yarn turbo run build --filter ${appEnv}`, { stdio: "inherit" });
