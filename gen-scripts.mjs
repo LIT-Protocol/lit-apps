26584e3462514764222e5f6057173e8c1c930000
Ansonhkg <ansonox@gmail.com>
2023-09-07 15:05:08 +0100

    feat: add gen scripts

---
+++ gen-scripts.mjs
@@ -0,0 +1,46 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
+const appsDir = './apps';
+
+async function generateScripts(dryRun = false) {
+  try {
+    // Read the apps directory
+    const folders = await fs.readdir(appsDir, { withFileTypes: true });
+
+    // Filter for only directories
+    const appNames = folders
+      .filter(dirent => dirent.isDirectory())
+      .map(dirent => dirent.name);
+
+    // Generate scripts for each app
+    const scripts = {};
+
+    for (const appName of appNames) {
+      const deleteCommands = appNames
+        .filter(name => name !== appName)
+        .map(name => `rm -rf apps/${name}`)
+        .join(' && ');
+
+      scripts[`cloud:build:${appName}`] = `${deleteCommands} && yarn && yarn build`;
+    }
+
+    if (dryRun) {
+      // If dry run, just log the scripts
+      console.log(scripts);
+    } else {
+      // Otherwise, update the package.json file
+      const packageJsonPath = path.join(process.cwd(), 'package.json');
+      const packageJson = JSON.parse(await fs.readFile(packageJsonPath, 'utf-8'));
+      packageJson.scripts = { ...packageJson.scripts, ...scripts };
+      await fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2));
+      console.log('Updated package.json with new scripts.');
+    }
+  } catch (error) {
+    console.error('Error generating scripts:', error);
+  }
+}
+
+// Check for the --dry-run argument
+const dryRun = process.argv.includes('--dry-run');
+generateScripts(dryRun);
