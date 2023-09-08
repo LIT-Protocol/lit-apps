d3bc11eb5146895e8c05d263510a080bbc69f444
Ansonhkg <ansonox@gmail.com>
2023-09-08 13:43:43 +0100

    chore: add deployer address

--- apps/lit-general-worker/src/aggregator.ts
+++ apps/lit-general-worker/src/aggregator.ts
@@ -25,6 +25,7 @@ let lastUpdated: Date | null = null;
 
 // https://github.com/LIT-Protocol/lit-assets/tree/develop/blockchain/contracts/contracts/lit-node
 const contracts = [
+  "AccessControlConditions",
   "Allowlist",
   "ConditionValidations",
   "HDKeyDeriver",
@@ -49,6 +50,7 @@ const DEPLOYER_ADDRESSES = [
   "0x046bf7bb88e0e0941358ce3f5a765c9acdda7b9c",
   "0xa54b67b7d202f0516c340e18169882ec9b6f88d8",
   "0xe964c013414d2d2c34c9bd319a52cf334e8bddb7",
+  "0xD875480fdE946c74ecaF71bb643cbe9F2E68E5a8",
 
   // If user provide it in process.env, then combine it with the default ones
   ...(process.env.DEPLOYER_ADDRESSES || "").split(","),
@@ -62,6 +64,50 @@ aggregator.get("/contract-addresses", (req, res) => {
       .status(500)
       .json({ success: false, message: "Cache not ready yet" });
   }
+});
+
+aggregator.get("/serrano-contract-addresses", (req, res) => {
+  return res.json({
+    success: true,
+    data: [
+      {
+        name: "AccessControlConditions",
+        contracts: [
+          {
+            address_hash: "0x8b353Bb9E26F2c2B8155f377982537C39AD01A1B",
+            inserted_at: "2023-04-27T00:00:00.000000Z",
+          },
+        ],
+      },
+      {
+        name: "PKPNFT",
+        contracts: [
+          {
+            address_hash: "0x8F75a53F65e31DD0D2e40d0827becAaE2299D111",
+            inserted_at: "2023-04-27T00:00:00.000000Z",
+          },
+        ],
+      },
+      {
+        name: "PKPHelper",
+        contracts: [
+          {
+            address_hash: "0x8bB62077437D918891F12c7F35d9e1B78468bF11",
+            inserted_at: "2023-04-27T00:00:00.000000Z",
+          },
+        ],
+      },
+      {
+        name: "PKPPermissions",
+        contracts: [
+          {
+            address_hash: "0x4Aed2F242E806c58758677059340e29E6B5b7619",
+            inserted_at: "2023-04-27T00:00:00.000000Z",
+          },
+        ],
+      },
+    ],
+  });
 });
 
 // Update cache immediately when the server starts
