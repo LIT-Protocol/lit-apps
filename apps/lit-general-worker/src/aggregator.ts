5ffe50c8553af7faa3f111eabf8f78b130195f40
Ansonhkg <ansonox@gmail.com>
2023-10-03 00:23:46 +0100

    update mapper

--- apps/lit-general-worker/src/aggregator.ts
+++ apps/lit-general-worker/src/aggregator.ts
@@ -4,73 +4,44 @@ import bodyParser from "body-parser";
 const aggregator = express();
 aggregator.use(bodyParser.json());
 
-type ContractInfo = {
-  address_hash: string;
-  block_hash: string | null;
-  block_number: number;
-  holder_count: number | null;
-  inserted_at: string; // You might want to use Date if you're going to convert this string to a Date object
-  name: string;
-  symbol: string | null;
-  tx_hash: string | null;
-  type: "contract"; // Assuming 'type' will always have the value "contract"
-  ABIUrl: string | null;
-  creator: string | null;
-};
-
 let cache = {
   cayenne: null,
   serrano: null,
 };
-let lastUpdated: Date | null = null;
-
-
-
 
 // -- config
 const mapper = {
   // -- Token
-  'litTokenContractAddress': 'LITToken',
+  litTokenContractAddress: "LITToken",
 
   // -- PKPs
-  'pkpNftContractAddress': 'PKPNFT',
-  'pkpHelperContractAddress': 'PKPHelper',
-  'pkpPermissionsContractAddress': 'PKPPermissions',
-  'pkpNftMetadataContractAddress': 'PKPNFTMetadata',
-  'pubkeyRouterContractAddress': 'PubkeyRouter',
+  pkpNftContractAddress: "PKPNFTDiamond",
+  pkpHelperContractAddress: "PKPHelperDiamond",
+  pkpPermissionsContractAddress: "PKPPermissionsDiamond",
+  pkpNftMetadataContractAddress: "PKPNFTMetadata",
+  pubkeyRouterContractAddress: "PubkeyRouterDiamond",
 
-  // -- 
-  "stakingBalancesContractAddress": "StakingBalances",
-  "stakingContractAddress": "Staking",
-  "multisenderContractAddress": "Multisender",
+  // --
+  stakingBalancesContractAddress: "StakingBalances",
+  stakingContractAddress: "Staking",
+  multisenderContractAddress: "Multisender",
 
   // -- Rate Limit NFT
-  "rateLimitNftContractAddress": "RateLimitNFT",
-  "allowlistContractAddress": "Allowlist",
-  "resolverContractAddress": "Resolver",
+  rateLimitNftContractAddress: "RateLimitNFTDiamond",
+  allowlistContractAddress: "Allowlist",
+  resolverContractAddress: "Resolver",
 
   // -- Domain Wallet
-  "DomainWaleltRegistryAddress": "DomainWaleltRegistry",
-  "DomainWalletOracleAddress": "DomainWalletOracle",
-  "hdKeyDeriverContractAddress": "HDKeyDeriver",
-}
+  DomainWaleltRegistryAddress: "DomainWaleltRegistry",
+  DomainWalletOracleAddress: "DomainWalletOracle",
+  hdKeyDeriverContractAddress: "HDKeyDeriver",
+};
 
-// const CONTRACT_API = `https://chain.litprotocol.com/token-autocomplete?q=`;
 const LOOKUP_API = `https://chain.litprotocol.com/api?module=account&action=txlist&address=`;
 const ABI_API = `https://chain.litprotocol.com/api?module=contract&action=getabi&address=`;
 
 const CAYENNE_CONTRACTS_JSON = 'https://raw.githubusercontent.com/LIT-Protocol/networks/main/cayenne/deployed-lit-node-contracts-temp.json';
 const SERRANO_CONTRACTS_JSON = 'https://raw.githubusercontent.com/LIT-Protocol/networks/main/serrano/deployed-lit-node-contracts-temp.json';
-
-// const DEPLOYER_ADDRESSES = [
-//   "0x046bf7bb88e0e0941358ce3f5a765c9acdda7b9c",
-//   "0xa54b67b7d202f0516c340e18169882ec9b6f88d8",
-//   "0xe964c013414d2d2c34c9bd319a52cf334e8bddb7",
-//   "0xd875480fde946c74ecaf71bb643cbe9f2e68e5a8",
-
-//   // If user provide it in process.env, then combine it with the default ones
-//   ...(process.env.DEPLOYER_ADDRESSES || "").split(","),
-// ];
 
 aggregator.get("/contract-addresses", (req, res) => {
   if (cache.cayenne !== null && cache.cayenne.length > 0) {
