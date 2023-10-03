6e380719536f931f61114cb4875a42031137922a
Ansonhkg <ansonox@gmail.com>
2023-10-03 14:29:19 +0100

    github accs token

--- apps/lit-general-worker/src/aggregator.ts
+++ apps/lit-general-worker/src/aggregator.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
-import * as dotenv from "dotenv";
-dotenv.config();
+// import * as dotenv from "dotenv";
+// dotenv.config();
 
 // https://github.com/LIT-Protocol/lit-assets/tree/develop/rust/lit-core/lit-blockchain/abis
 // -- config
