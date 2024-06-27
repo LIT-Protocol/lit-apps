a58d31a09925e94b5c92c19aa49b969b77ae989c
Anson <ansonox@gmail.com>
2024-06-27 17:49:11 +0100

    fix: use minimum required staking abi

--- apps/lit-general-worker/src/handlers/contracts-handler/datil-dev/StakingAbi.ts
+++ apps/lit-general-worker/src/handlers/contracts-handler/datil-dev/StakingAbi.ts
@@ -1,230 +1,97 @@
 export const StakingABI = [
   {
+    inputs: [],
+    name: "currentValidatorCountForConsensus",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getValidatorsInCurrentEpoch",
+    outputs: [
+      {
+        internalType: "address[]",
+        name: "",
+        type: "address[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getKickedValidators",
+    outputs: [
+      {
+        internalType: "address[]",
+        name: "",
+        type: "address[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
     inputs: [
+      {
+        internalType: "address[]",
+        name: "addresses",
+        type: "address[]",
+      },
+    ],
+    name: "getValidatorsStructs",
+    outputs: [
       {
         components: [
           {
+            internalType: "uint32",
+            name: "ip",
+            type: "uint32",
+          },
+          {
+            internalType: "uint128",
+            name: "ipv6",
+            type: "uint128",
+          },
+          {
+            internalType: "uint32",
+            name: "port",
+            type: "uint32",
+          },
+          {
             internalType: "address",
-            name: "facetAddress",
+            name: "nodeAddress",
             type: "address",
           },
           {
-            internalType: "enum IDiamond.FacetCutAction",
-            name: "action",
-            type: "uint8",
+            internalType: "uint256",
+            name: "reward",
+            type: "uint256",
           },
           {
-            internalType: "bytes4[]",
-            name: "functionSelectors",
-            type: "bytes4[]",
+            internalType: "uint256",
+            name: "senderPubKey",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "receiverPubKey",
+            type: "uint256",
           },
         ],
-        internalType: "struct IDiamond.FacetCut[]",
-        name: "_diamondCut",
+        internalType: "struct LibStakingStorage.Validator[]",
+        name: "",
         type: "tuple[]",
       },
-      {
-        components: [
-          {
-            internalType: "address",
-            name: "owner",
-            type: "address",
-          },
-          {
-            internalType: "address",
-            name: "init",
-            type: "address",
-          },
-          {
-            internalType: "bytes",
-            name: "initCalldata",
-            type: "bytes",
-          },
-          {
-            internalType: "address",
-            name: "contractResolver",
-            type: "address",
-          },
-          {
-            internalType: "enum ContractResolver.Env",
-            name: "env",
-            type: "uint8",
-          },
-        ],
-        internalType: "struct StakingArgs",
-        name: "_args",
-        type: "tuple",
-      },
     ],
-    stateMutability: "payable",
-    type: "constructor",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes4",
-        name: "_selector",
-        type: "bytes4",
-      },
-    ],
-    name: "CannotAddFunctionToDiamondThatAlreadyExists",
-    type: "error",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes4[]",
-        name: "_selectors",
-        type: "bytes4[]",
-      },
-    ],
-    name: "CannotAddSelectorsToZeroAddress",
-    type: "error",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes4",
-        name: "_selector",
-        type: "bytes4",
-      },
-    ],
-    name: "CannotRemoveFunctionThatDoesNotExist",
-    type: "error",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes4",
-        name: "_selector",
-        type: "bytes4",
-      },
-    ],
-    name: "CannotRemoveImmutableFunction",
-    type: "error",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes4",
-        name: "_selector",
-        type: "bytes4",
-      },
-    ],
-    name: "CannotReplaceFunctionThatDoesNotExists",
-    type: "error",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes4",
-        name: "_selector",
-        type: "bytes4",
-      },
-    ],
-    name: "CannotReplaceFunctionWithTheSameFunctionFromTheSameFacet",
-    type: "error",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes4[]",
-        name: "_selectors",
-        type: "bytes4[]",
-      },
-    ],
-    name: "CannotReplaceFunctionsFromFacetWithZeroAddress",
-    type: "error",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes4",
-        name: "_selector",
-        type: "bytes4",
-      },
-    ],
-    name: "CannotReplaceImmutableFunction",
-    type: "error",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes4",
-        name: "_functionSelector",
-        type: "bytes4",
-      },
-    ],
-    name: "FunctionNotFound",
-    type: "error",
-  },
-  {
-    inputs: [
-      {
-        internalType: "uint8",
-        name: "_action",
-        type: "uint8",
-      },
-    ],
-    name: "IncorrectFacetCutAction",
-    type: "error",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "_initializationContractAddress",
-        type: "address",
-      },
-      {
-        internalType: "bytes",
-        name: "_calldata",
-        type: "bytes",
-      },
-    ],
-    name: "InitializationFunctionReverted",
-    type: "error",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "_contractAddress",
-        type: "address",
-      },
-      {
-        internalType: "string",
-        name: "_message",
-        type: "string",
-      },
-    ],
-    name: "NoBytecodeAtAddress",
-    type: "error",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "_facetAddress",
-        type: "address",
-      },
-    ],
-    name: "NoSelectorsProvidedForFacetForCut",
-    type: "error",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "_facetAddress",
-        type: "address",
-      },
-    ],
-    name: "RemoveFacetAddressMustBeZeroAddress",
-    type: "error",
-  },
-  {
-    stateMutability: "nonpayable",
-    type: "fallback",
+    stateMutability: "view",
+    type: "function",
   },
 ];
