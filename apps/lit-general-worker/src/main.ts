e32f0afffcd51e7752731a70b00ac7b551bafef4
Anson <ansonox@gmail.com>
2024-07-30 15:24:08 +0100

    feat: upping the rate limit

--- apps/lit-general-worker/src/main.ts
+++ apps/lit-general-worker/src/main.ts
@@ -5,8 +5,8 @@ import { contractsHandler } from "./handlers/contracts-handler/contractsHandler"
 import { rateLimit } from "express-rate-limit";
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  windowMs: 1 * 60 * 1000, // 1 minute
+  limit: 60, // Limit each IP to 60 requests per `window` (here, per 1 minutes).
   standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
   // store: ... , // Redis, Memcached, etc. See below.
