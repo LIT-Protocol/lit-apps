67cba52ea65c7b9ff0bbc46ab13867b202617fb3
Anson <ansonox@gmail.com>
2024-07-30 17:57:12 +0100

    fix: ip getting

--- apps/lit-general-worker/src/main.ts
+++ apps/lit-general-worker/src/main.ts
@@ -4,6 +4,15 @@ import express, { Express, Request, Response, NextFunction } from "express";
 import { contractsHandler } from "./handlers/contracts-handler/contractsHandler";
 import { rateLimit } from "express-rate-limit";
 
+const getClientIp = (req: Request): string => {
+  const forwardedFor = req.headers["x-forwarded-for"];
+  if (typeof forwardedFor === "string") {
+    // Get the first IP in the list (client's original IP)
+    return forwardedFor.split(",")[0].trim();
+  }
+  return req.ip || "Unknown";
+};
+
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   limit: 60, // Limit each IP to 60 requests per `window` (here, per 1 minutes).
@@ -11,8 +20,10 @@ const limiter = rateLimit({
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
   // store: ... , // Redis, Memcached, etc. See below.
   handler: (req, res) => {
-    const clientIp = req.headers["x-forwarded-for"] || req.ip;
-    console.log(`❗️ Too many requests from ${typeof clientIp} ${clientIp}`);
+    const clientIP = getClientIp(req);
+    // ❗️ Too many requests from string 199.212.41.25, 10.213.41.231
+
+    console.log(`❗️ Too many requests from ${typeof clientIP} ${clientIP}`);
 
     res
       .status(429)
@@ -37,7 +48,7 @@ console.log(`Loaded ${bannedIPs.size} banned IPs`);
 
 // Middleware to check for banned IPs
 const checkBannedIP = (req: Request, res: Response, next: NextFunction) => {
-  const clientIP = (req.headers["x-forwarded-for"] || req.ip) as string;
+  const clientIP = getClientIp(req);
   if (bannedIPs.has(clientIP)) {
     console.log(`Blocked request from banned IP: ${clientIP}`);
     return res.status(403).send("Access Denied");
